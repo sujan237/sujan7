278d9f43c02c3ca23a0e43e6e7202365d4642274
sujan237 <sujanjungshahi7@gmail.com>
2024-07-25 19:46:11 +0545

    more lements

--- pages/visual.py
+++ pages/visual.py
@@ -1,8 +1,18 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
 
 with st.container():
    st.write("Visuaization")
 
    # You can call any Streamlit command, including custom components:
    st.bar_chart(np.random.randn(50, 3))
+
+   chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+   st.line_chart(chart_data)
+
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(df)
